9a3da229d642cc3cf775750bb9046db3f5a5a4d6
Mark Hepburn <Mark.Hepburn@gmail.com>
2015-02-01 17:34:09 +1100

    Skip release signing for now

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-oauth "1.4.0"]
-                 [cheshire "5.0.1"]])
+                 [cheshire "5.0.1"]]
+
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo/"
+                                    :sign-releases false}]])
